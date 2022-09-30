0feacdd0704be12d04567d5dc099d4707101f3ba
Fayrouz Mohamed <67238688+TurquoiseApps@users.noreply.github.com>
2022-09-30 15:45:01 +0200

    Update Java Stdin and Stdout II.java

--- Java Stdin and Stdout II.java
+++ Java Stdin and Stdout II.java
@@ -4,6 +4,11 @@ https://www.hackerrank.com/challenges/java-stdin-stdout/problem
 
 In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions in the Output Format section below. 
 To make the problem a little easier, a portion of the code is provided for you in the editor.
+
+NOTE:
+Note: If you use the nextLine() method immediately following the nextInt() method, recall that nextInt() reads integer tokens;
+because of this, the last newline character for that line of integer input is still queued in the input buffer and the next nextLine() 
+will be reading the remainder of the integer line (which is empty).
 */
 
 
