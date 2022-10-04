69b553f6376ca944be8157717c4d51d5b553446d
Fayrouz Mohamed <67238688+TurquoiseApps@users.noreply.github.com>
2022-10-04 11:56:45 +0200

    Update 1- Java If-Else.java

--- 1- Java If-Else.java
+++ 1- Java If-Else.java
@@ -3,7 +3,40 @@ Problem statment:
 https://www.hackerrank.com/challenges/java-if-else/problem
 
 In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes.
-An if-else statement has the following logical flow
+An if-else statement has the following logical flow:
+
+Task
+Given an integer, n, perform the following conditional actions :
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+
+Input Format
+A single line containing a positive integer, n.
+
+Constraints
+1 <= n <= 100
+
+Output Format
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+Sample input 0
+3
+ 
+Sample output 0
+Weird
+ 
+Sample input 1
+24
+ 
+Sample output 1
+Not Weird
+
+Explanation
+Sample Case 0: n=3, where n is odd and odd numbers are weird, so we print Weird.
+Sample Case 1: n=24, where n>20 and n is even, so it isn’t weird. Thus, we print Not Weird.
 */
 
 
