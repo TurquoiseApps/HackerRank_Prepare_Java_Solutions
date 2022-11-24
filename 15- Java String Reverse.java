3e7e80f2cd59ae3a77721e68e6573a375da096cb
Fayrouz Mohamed <67238688+fayrouz10@users.noreply.github.com>
2022-11-24 17:10:42 +0200

    Create 15- Java String Reverse.java

---
+++ 15- Java String Reverse.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        // get the inputs
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String result;
+        
+        // create a string builder and reverse the string
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        
+        // compare the original and the reversed strings and show the result
+        result = s.equals(sb.toString())? "Yes": "No";
+        System.out.println(result);
+        
+    }
+    
+    
+}
