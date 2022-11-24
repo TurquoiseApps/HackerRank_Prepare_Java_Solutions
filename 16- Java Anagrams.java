25d08b286db0a14368ca803b25de31e47f490bd6
Fayrouz Mohamed <67238688+fayrouz10@users.noreply.github.com>
2022-11-24 17:32:47 +0200

    Create 16- Java Anagrams.java

---
+++ 16- Java Anagrams.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+            // get the inputs
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        String result;
+        
+        // convert the strings into char arrays
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        char[] array1 = a.toCharArray();
+        char[] array2 = b.toCharArray();
+        
+        // sort the two arrays and compare
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+        
+        a = String.valueOf(array1);
+        b = String.valueOf(array2);
+        
+        result = a.equals(b)? "Anagrams": "Not Anagrams";
+        System.out.println(result);
+
+        
+    }
+}
