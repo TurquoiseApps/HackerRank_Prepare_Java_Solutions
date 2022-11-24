072fbea85141fadd6556d4e907055563f827de81
Fayrouz Mohamed <67238688+fayrouz10@users.noreply.github.com>
2022-11-24 17:08:07 +0200

    Create 12- Java Strings Introduction.java

---
+++ 12- Java Strings Introduction.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+         System.out.println(A.length()+B.length());
+          int value = A.compareTo(B);
+          String result = (value>0?"Yes":"No");
+          System.out.println(result);
+          
+          String upperCasedA = A.substring(0,1).toUpperCase()+A.substring(1);      
+          String upperCasedB = B.substring(0,1).toUpperCase()+B.substring(1);      
+          
+          System.out.println(upperCasedA+" "+upperCasedB);
+                
+        
+    }
+}
+
+
+
